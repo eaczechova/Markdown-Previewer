1efd9676415abd1aa75057c781e27c5af4c5dbef
Ewa Czech <eaprojekty@gmail.com>
2019-04-04 23:01:17 +0200

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-## User Stories
-
-User Story: I can type GitHub-styled Markdown into the text area
-
-User Story: I can see the preview of the output in the markdown that is updated as the text is typed
-
-Project link: (https://www.freecodecamp.com/challenges/build-a-markdown-previewer)
---
+++ precache-manifest.ca0e542e522d82ad1f0a45efb311a6e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5b1733e4e2bf3b997b9",
+    "url": "/Markdown-Previewer/static/js/runtime~main.d5b1733e.js"
+  },
+  {
+    "revision": "cde82a2074cbe1fb23dd",
+    "url": "/Markdown-Previewer/static/js/main.cde82a20.chunk.js"
+  },
+  {
+    "revision": "3bb1885a2aed48ebf39a",
+    "url": "/Markdown-Previewer/static/js/2.3bb1885a.chunk.js"
+  },
+  {
+    "revision": "cde82a2074cbe1fb23dd",
+    "url": "/Markdown-Previewer/static/css/main.615b3a35.chunk.css"
+  },
+  {
+    "revision": "c6e7e51301c6a0bb4565a3cebd35ad06",
+    "url": "/Markdown-Previewer/index.html"
+  }
+];
