c7f9e10a6a75228eeb3c4f83a8934cf2bb02d929
Ewa Czech <eaprojekty@gmail.com>
2019-04-04 09:49:03 +0200

    add new style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const marked = require("marked");
+
+let placholderMessage = `
+# This is React Markdown Previewer Project
+## This is a sub-heading...
+### And here's some other cool stuff:
+Heres some code
+`;
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      text: placholderMessage
     };
-
     this.onChange = this.onChange.bind(this);
   }
 
@@ -17,9 +25,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <textarea id="editor" onChange={this.onChange}></textarea>
-        <div id="preview">{this.state.text}</div>
+      <div className="app">
+        <div className = "app__content">
+          <div id="editor__container">
+            <div id="editor__heading">#Editor</div>
+            <textarea id="editor" value={this.state.text} onChange={this.onChange}></textarea>
+          </div>
+          <div id="app__division"></div>
+          <div id="preview">
+            <div id="preview__heading">#Preview</div>
+          <div id="preview__view" dangerouslySetInnerHTML = {{__html: marked(this.state.text)}}></div>
+        </div>
+        </div>
       </div>
     );
   }
